33bfb051fcf14b743c10f6ce67045e7a08732e28
Shelin Anton <shelinanton@gmail.com>
2019-05-02 02:46:56 +0300

    trying more functional way

--- FsForth/Forth.fs
+++ FsForth/Forth.fs
@@ -8,133 +8,192 @@ open System.Runtime.CompilerServices
 open System.Text
 open System.IO
 
-type BaseType = Int32
-type FnPointer = BaseType
-let baseSize = sizeof<BaseType>
-let pad address = (address + (baseSize-1)) &&& ~~~(baseSize-1)//same as address + (baseSize - (string_length % baseSize)) % baseSize
+module Memory = 
 
-let inc (v:BaseType byref) count = v <- v + count
-let dec (v:BaseType byref) count = v <- v - count
-type ByteString = { address: BaseType; length: BaseType }
-type Memory(memory : BaseType[]) =
-    let mutable memoryTop = 0
-    let asByteSpan () = MemoryMarshal.Cast<BaseType,byte>(new Span<BaseType>(memory))
-    let asByteReadOnlySpan () = MemoryMarshal.Cast<BaseType,byte>(new ReadOnlySpan<BaseType>(memory))
+    [<Literal>]
+    let baseSize = 4//sizeof<Int32>
+    [<Measure>] type bytes
+    [<Measure>] type ints
+    //conversion factor
+    let bytesPerInt = 4<bytes/ints>  
+    let toBytes (ints:int<ints>) : int<bytes>= ints * bytesPerInt
+    let toInts (bytes:int<bytes>) : int<ints>= bytes / bytesPerInt
+
+    let pad address = (address + (baseSize-1)) &&& ~~~(baseSize-1)//same as address + (baseSize - (string_length % baseSize)) % baseSize
+
+    type MemoryConfig = {
+        INITIAL_DATA_SEGMENT_SIZE : int<bytes>
+        DATA_STACK_SIZE : int<bytes>
+        RETURN_STACK_SIZE : int<bytes>
+        BUFFER_SIZE : int<bytes>
+    }
+
+    let defaultConfig = {
+        INITIAL_DATA_SEGMENT_SIZE = 65536<bytes>
+        DATA_STACK_SIZE = 8192<bytes>
+        RETURN_STACK_SIZE  = 8192<bytes>
+        BUFFER_SIZE = 4096<bytes>
+    }
+    type Span = private {
+        address : int<bytes>
+        size : int<bytes>
+    }
+
+    let top span = span.address + span.size
+
+    type Cursor = private {
+        span : Span
+        isInverse : bool
+        mutable current : int<bytes>
+    }
+    let reset cursor = cursor.current <- if cursor.isInverse then cursor.span.address + cursor.span.size else cursor.span.address
+    let used cursor = if cursor.isInverse then top cursor.span - cursor.current else cursor.current - cursor.span.address
+    let isFull cursor = used cursor = cursor.span.size
+    let createCursor address size isInverse = 
+        let c ={ 
+            span = { address = address; size = size }
+            isInverse = isInverse
+            current = 0<bytes>
+        }
+        reset c
+        c
+
+
+    let validate (cursor:Cursor) = ()
+
+        //if cursor.current < cursor.span.address || cursor.current > (top cursor.span)
+        //then failwith "broken span bounds"
+
+    let change op noop (v:Cursor) count = 
+        let prev = v.current
+        let op = if v.isInverse then op else noop
+        v.current <- op v.current count
+        validate v
+        prev
+
+    let inc = change (-) (+)
+    let dec = change (+) (-) 
+
+    let incInts span ints = inc span (toBytes ints)
+    let decInts span ints = dec span (toBytes ints)
+
+    type Memory = private {
+        memory : byte[]
+    }
+
     
-    member x.MemoryTop = memoryTop
 
-    member x.get offset = memory.[offset/baseSize]
+    let get (memory:Memory)  (address:int<bytes>) = memory.memory.[int address]
+    let set (memory:Memory)  (address:int<bytes>) v = memory.memory.[int address] <- v 
 
-    member x.set offset v = memory.[offset/baseSize] <- v 
+    let getInt (memory:Memory) (address:int<bytes>) = BitConverter.ToInt32(memory.memory, address |> int)
 
-    member x.copyFromBytes offset (arr:byte array) = arr.CopyTo(asByteSpan().Slice(offset, arr.Length) )
-    member x.getBytes offset length = asByteSpan().Slice(offset, length)
-    member x.writeStdout str = 
+    let setInt (memory:Memory) (address:int<bytes>) (v:int) = BitConverter.GetBytes(v).CopyTo(memory.memory, address |> int);
+
+    let copyFromBytes (memory:Memory) (address:int<bytes>) (arr:byte array) = arr.CopyTo(memory.memory, int address)
+    let writeStdout (memory:Memory) cursor = 
         use out = System.Console.OpenStandardOutput()
-        let span = asByteReadOnlySpan().Slice(str.address, str.length)
-        out.Write span
+        out.Write(memory.memory, int cursor.span.address, int cursor.current)
         out.Flush()
 
-    member x.readStdin str = 
+    let readStdin (memory:Memory) span = 
         use inp = System.Console.OpenStandardInput()
-        let span = asByteSpan().Slice(str.address, str.length)
-        inp.Read span
+        inp.Read(memory.memory, int span.address, int span.size) * 1<bytes>
+
+    let write (memory:Memory) cursor b = 
+        set memory cursor.current b
+        inc cursor 1<bytes> |> ignore
+
+    let writeInt (memory:Memory) cursor i = 
+        setInt memory cursor.current i
+        incInts cursor 1<ints> |> ignore
+
+    type InputBuffer(memory : Memory, cursor : Cursor)=
         
-    member x.reserveMemoryBytes size = 
-        let free = memoryTop
-        memoryTop <- memoryTop + size
-        free
- 
-    member x.getByte offset : BaseType = 
-        let memoryBytes = asByteSpan ()
-        int memoryBytes.[offset]
-    member x.setByte offset (v:BaseType) = 
-        let memoryBytes = asByteSpan ()
-        memoryBytes.[offset] <- byte v 
+        let mutable bufftop = cursor.span.address 
+        do 
+            if cursor.isInverse then failwith "not possible to use inverse memory as buffer"
+        member x.GetLast n = 
+            let count = used cursor
+            let count = if n > count then count else n
+            { address = cursor.current - count; size = count}
 
+        member x.get () = 
+            if cursor.current < bufftop
+            then let v = get memory cursor.current
+                 inc cursor 1<bytes> |> ignore
+                 v
+            else let count = readStdin memory cursor.span 
+                 if count<= 0<bytes> then failwith "exit"//todo
+                 else bufftop <- cursor.span.address + count 
+                      reset cursor
+                      x.get ()
 
-type Variable(memory:Memory, initial: BaseType)=
-    let addr = memory.reserveMemoryBytes baseSize
-    do
-        memory.set addr initial
-    member x.address = addr
-    member x.get () = memory.get addr
-    member x.set v = memory.set addr v
+    type OutputBuffer(memory : Memory, cursor : Cursor)=
 
+        member x.flush() =
+            writeStdout memory cursor
+            reset cursor
 
-let reserveString (memory:Memory) (str:byte[]) = 
-    let buffer = memory.reserveMemoryBytes str.Length
-    memory.copyFromBytes buffer str
-    { address= buffer; length= str.Length }
+        member x.set c = 
+            if isFull cursor then x.flush()
+            write memory cursor c
 
-type StringBuffer(memory : Memory, BUFFER_SIZE:int)=
-    let buffer = memory.reserveMemoryBytes BUFFER_SIZE
-    let mutable bufftop = buffer //buffer pointer
-    member x.str = {address= buffer; length = bufftop - buffer}
-    member x.reset () = bufftop <- buffer
-    member x.write c = memory.setByte bufftop c
-                       inc &bufftop 1
-
-type InputBuffer(memory : Memory, BUFFER_SIZE:int)=
-    let buffer = memory.reserveMemoryBytes BUFFER_SIZE
-    let mutable bufftop = buffer //buffer pointer
-    let mutable currkey  = buffer //next character to read
+    type Stack(memory : Memory, cursor : Cursor)=
+        do 
+            if not cursor.isInverse then failwith "not possible to use non inverse memory as stack"
+        member x.reset () = reset cursor
+        member x.top 
+            with get () = cursor.current
+            and set (value) = cursor.current <- value
+        member x.S0 = top cursor.span
+        member x.push v = 
+            assert (cursor.current > cursor.span.address)
+            decInts cursor 1<ints> |> ignore
+            setInt memory cursor.current v
     
-    member x.GetLast n = 
-        let count = currkey - buffer
-        let count = if n > count then count else n
-        { address= currkey - count; length = count}
+        member x.pop () = 
+            assert (cursor.current < top cursor.span)
+            let v = get memory cursor.current
+            incInts cursor 1<ints> |> ignore
+            v
 
-    member x.get () = 
-        if currkey < bufftop 
-        then let v = memory.getByte currkey
-             currkey <- currkey + 1
-             v
-        else let count = memory.readStdin {address= buffer ; length = BUFFER_SIZE}
-             if count<= 0 then failwith "exit"//todo
-             else bufftop <- buffer + count
-                  currkey <- buffer
-                  x.get ()
-
-type OutputBuffer(memory : Memory, BUFFER_SIZE:int)=
-    let buffer = memory.reserveMemoryBytes BUFFER_SIZE
-    let mutable currkey = buffer //next character to write
-
-    member x.flush() =
-        memory.writeStdout {address = buffer; length= (currkey-buffer)}
-        currkey<-0
-
-    member x.set c = 
-        if currkey >= buffer + BUFFER_SIZE then x.flush()
-        memory.setByte currkey c
-        currkey <- currkey + 1
-
-type Stack(memory : Memory, size:int)=
-    let low = memory.reserveMemoryBytes size//growing backward
-    let high = low + size
-    let mutable sp = high //stack pointer
-
-    member x.reset () = sp <- high
-    member x.top 
-        with get () = sp
-        and set (value) = sp <- value
-    member x.S0 = high
-    member x.push v = 
-        assert (sp > low)
-        dec &sp baseSize
-        memory.set sp v
+        member x.peek (offset:int<ints>) = getInt memory (cursor.current + (toBytes offset))
     
-    member x.pop () = 
-        assert (sp < high)
-        let v = memory.get sp
-        inc &sp baseSize
-        v
+        member x.apply f = x.peek 0<ints> |> f |> setInt memory cursor.current 
 
-    member x.peek offset = memory.get (sp - (offset * baseSize))
-    
-    member x.apply f = x.peek 0 |> f |> memory.set sp 
+    type MemoryLayout = {
+        memory : Memory
+        allocated : Cursor
+        data : Cursor
+        data_stack : Stack
+        return_stack : Stack
+        input_buffer : InputBuffer
+        out_buffer : OutputBuffer
+        word_buffer : Cursor
+    }
+
+    let create (size:int<bytes>) config  = 
+        let memory = { memory =Array.create (int size) 0uy }
+        let allocated = createCursor 0<bytes> size false 
+        let allocate bytes isInverse init = 
+            let address = inc allocated bytes
+            let c = createCursor address bytes isInverse
+            init (memory,c)
+
+        {
+            memory = memory
+            allocated = allocated
+            data = allocate config.INITIAL_DATA_SEGMENT_SIZE false snd
+            data_stack = allocate config.DATA_STACK_SIZE true Stack
+            return_stack = allocate config.RETURN_STACK_SIZE true Stack
+            input_buffer = allocate config.BUFFER_SIZE false InputBuffer
+            out_buffer= allocate config.BUFFER_SIZE false OutputBuffer
+            word_buffer = allocate 32<bytes> false snd
+        }
 
 
+type FnPointer = Int32
 
 type ForthVM(memory : Memory) =
     //memory for main forth variables
@@ -142,18 +201,14 @@ type ForthVM(memory : Memory) =
     member val STATE = Variable(memory, 0)//Is the interpreter executing code (0) or compiling a word (non-zero)?
     member val LATEST = Variable(memory, 0)//Points to the latest (most recently defined) word in the dictionary.
     member val HERE = Variable(memory, 0)//Points to the next free byte of memory.  When compiling, compiled words go here.
-    member val S0 = Variable(memory, 0)//Stores the address of the top of the parameter stack.
-    member val R0 = Variable(memory, 0)//Stores the address of the top of the parameter stack.
-    member val BASE = Variable(memory, 10)//The current base for printing and reading numbers.
-    member val SP = Stack(memory, 8192)//data stack
-    member val RSP = Stack(memory, 8192)//return stack
-    member val Input = InputBuffer(memory, 4096)//stdin
-    member val Output = OutputBuffer(memory, 4096)//stdout
-    member val WordBuffer = StringBuffer(memory, 32)//words storage
+    //member val S0 = Variable(memory, 0)//Stores the address of the top of the parameter stack.
+    //member val R0 = Variable(memory, 0)//Stores the address of the top of the parameter stack.
+    //member val BASE = Variable(memory, 10)//The current base for printing and reading numbers.
+   
     member val  errmsg = reserveString memory <| Encoding.ASCII.GetBytes "PARSE ERROR: "
     member val  errmsgnl = reserveString memory <| Encoding.ASCII.GetBytes "\n"
-    [<DefaultValue>] val mutable IP : BaseType//istruction pointer in bytes
-    [<DefaultValue>] val mutable W : BaseType//work
+    [<DefaultValue>] val mutable IP : Int32//istruction pointer in bytes
+    [<DefaultValue>] val mutable W : Int32//work
     member x.memory = memory
 
 type Fn = ForthVM -> FnPointer
@@ -203,7 +258,7 @@ type Flags =
 
 let LENMASK = 0x1f
 type ReadMode = SKIP|WORD|COMMENT
-type Forth(memory : BaseType[]) =
+type Forth(memory : Int32[]) =
     let memory = Memory(memory)
     let code = CodeMemory()
     let vm = ForthVM(memory)
@@ -216,7 +271,7 @@ type Forth(memory : BaseType[]) =
         vm.memory.setByte address <| int v
         vm.HERE.set <| address + 1
 
-    let write (v:BaseType) = 
+    let write (v:Int32) = 
         let address = vm.HERE.get()
         vm.memory.set address v
         vm.HERE.set <| address + baseSize
@@ -225,7 +280,7 @@ type Forth(memory : BaseType[]) =
 
     let align () = vm.HERE.get() |> pad |> vm.HERE.set
 
-    let rec run (fnIdx:BaseType) =  
+    let rec run (fnIdx:Int32) =  
         let f = code.get fnIdx
         let nextFn = f vm
         run nextFn
@@ -242,7 +297,7 @@ type Forth(memory : BaseType[]) =
         writeString name
         align()
 
-    member x.defwordRetCodeword (name: string) (flags:Flags) (program:BaseType[]) = 
+    member x.defwordRetCodeword (name: string) (flags:Flags) (program:Int32[]) = 
         x.create name flags 
         let codewordAddr = vm.memory.MemoryTop
         write code.NEST //codeword
@@ -250,7 +305,7 @@ type Forth(memory : BaseType[]) =
             write d 
         codewordAddr
 
-    member x.defword (name: string) (flags:Flags) (program:BaseType[]) = 
+    member x.defword (name: string) (flags:Flags) (program:Int32[]) = 
         x.defwordRetCodeword name flags program |>ignore
 
     //add native word
@@ -262,7 +317,7 @@ type Forth(memory : BaseType[]) =
     member x.defcode (name: string) (flags:Flags) (f:Fn) = x.defcodeRetAddr name flags f |> ignore
 
     //add variable
-    member x.defvarRetAddr (name: string) (flags:Flags) (varAddress:BaseType) (initial:BaseType option) = 
+    member x.defvarRetAddr (name: string) (flags:Flags) (varAddress:Int32) (initial:Int32 option) = 
         match initial with  
             | Some(initial) -> vm.memory.set varAddress initial
             | _ -> ()
@@ -270,14 +325,14 @@ type Forth(memory : BaseType[]) =
             vm.SP.push varAddress
             code.NEXT
         )
-    member x.defvar (name: string) (flags:Flags) (varAddress:BaseType) (initial:BaseType option) = x.defvarRetAddr name flags varAddress initial |> ignore
-    member x.defconstRetAddr (name: string) (flags:Flags) (value:BaseType) = 
+    member x.defvar (name: string) (flags:Flags) (varAddress:Int32) (initial:Int32 option) = x.defvarRetAddr name flags varAddress initial |> ignore
+    member x.defconstRetAddr (name: string) (flags:Flags) (value:Int32) = 
         x.defcodeRetAddr name flags (fun vm -> 
             vm.SP.push value
             code.NEXT
         )
 
-    member x.defconst (name: string) (flags:Flags) (value:BaseType) = 
+    member x.defconst (name: string) (flags:Flags) (value:Int32) = 
         x.defconstRetAddr name flags value |> ignore
 
     member x.coldStart () = 
@@ -355,7 +410,7 @@ type Forth(memory : BaseType[]) =
         ) 
 
         let apply (vm:ForthVM) = vm.SP.apply
-        let apply2 (vm:ForthVM) (f: BaseType->BaseType->BaseType)  = vm.SP.apply (f <| vm.SP.pop())
+        let apply2 (vm:ForthVM) (f: Int32->Int32->Int32)  = vm.SP.apply (f <| vm.SP.pop())
         let boolToBase b = if b then -1 else 0
     
         let applyBool vm f  = apply vm (fun a -> f a |> boolToBase) 
@@ -491,8 +546,8 @@ type Forth(memory : BaseType[]) =
         x.defvar "BASE" Flags.NONE vm.BASE.address Option.None
         
         x.defconst "VERSION" Flags.NONE 1
-        let RZ = x.defconstRetAddr "R0" Flags.NONE vm.R0.address
-        x.defconst "DOCOL" Flags.NONE code.NEST
+        let RZ = x.defconstRetAddr "R0" Flags.NONE vm.RSP.S0
+        x.defconst "DOCOL" Flags.NONE DOCOL
         x.defconst "F_IMMED" Flags.NONE <| int Flags.IMMEDIATE
         x.defconst "F_HIDDEN" Flags.NONE <| int Flags.HIDDEN
         x.defconst "F_LENMASK" Flags.NONE LENMASK
